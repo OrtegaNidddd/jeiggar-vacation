9adb36f2ba2a8d7b7b8bc3389bdd1ac5544df4a4
Nick Ortega <146968827+OrtegaNidddd@users.noreply.github.com>
2026-03-04 23:31:36 -0500

    style(landing): ajustar estilos y formato de componentes

--- src/features/landing/components/TravelCategories.tsx
+++ src/features/landing/components/TravelCategories.tsx
@@ -5,7 +5,7 @@ import { travelCategoriesLandingMock } from "../../../mocks/travel-categories-la
 export default function TravelCategories() {
     return (
         <div className="flex flex-col mx-auto max-w-6xl items-left gap-4 py-12">
-            <h2 className="text-2xl">{travelCategoriesLandingMock.title}</h2>
+            <h2 className="text-2xl font-bold">{travelCategoriesLandingMock.title}</h2>
             <p className="text-sm text-(--text-muted)! ">{travelCategoriesLandingMock.subtitle}</p>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                 {travelCategoriesLandingMock.categories.map((category, index) => (
